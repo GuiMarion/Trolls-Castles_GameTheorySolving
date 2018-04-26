ff774b576d124b31cb58d2fcaa6f970ea92b0f37
kalnar <kalnarobert@gmail.com>
2018-04-26 18:57:16 +0200

    integrate strategy elimination into Troll&Castles

--- Troll&Castles.py
+++ Troll&Castles.py
@@ -4,14 +4,13 @@ import time
 import numpy as np
 import logging
 
-
 # TIME : 1.74 seconds for (10,10,0)
 # TODO : Properly eliminate dominated strategies
 #         Try to implement a dynamic programming method in order to improve speed
 
 
 # We define the distance between the two castles.
-SIZE = 5
+SIZE = 7
 
 # This object contains all the states already computed in order not to compute them several times
 SEEN = {}
@@ -35,25 +34,41 @@ def rem(tab, k):
 			return tab
 
 
-def get_not_dominated_strategies(tab):
-	result_list = list(range(len(tab)))
+def eliminate_dominated_strategies(tab):
+	old_size = 0
+	tab = np.insert(tab, 0, range(1, tab.shape[0] + 1), axis=1)
+	tab = np.insert(tab, 0, range(0, tab.shape[1]), axis=0)
+	while tab.size is not old_size:
+		old_size = tab.size
+		tab = eliminate_strategies_in(tab).transpose()
+		tab = eliminate_strategies_in(tab, transposed=True).transpose()
+	return tab
 
-	strategy_index = 0
+
+def eliminate_strategies_in(tab, transposed=False):
+	to_remove = []
+
+	strategy_index = 1
 	while strategy_index < len(tab):
-		other_strategy_index = 0
+		other_strategy_index = 1
 		while other_strategy_index < len(tab):
-			enemy_strategy_index = 0
+			enemy_strategy_index = 1
 			same = True
 
 			if other_strategy_index == strategy_index:
 				other_strategy_index += 1
-				enemy_strategy_index = 0
+				enemy_strategy_index = 1
 
 			if other_strategy_index == len(tab):
-				return result_list
+				tab = np.delete(tab, to_remove, 0)
+				return tab
 
 			while enemy_strategy_index < len(tab[0]):
-				if tab[strategy_index][enemy_strategy_index] > tab[other_strategy_index][enemy_strategy_index]:
+				gain_player1 = tab[strategy_index][enemy_strategy_index]
+				gain_player2 = tab[other_strategy_index][enemy_strategy_index]
+				# if ((gain_player1 > gain_player2) != transposed) or ((gain_player1 < gain_player2) != (not transposed)):
+				not_dominated = gain_player1 < gain_player2 if transposed else gain_player1 > gain_player2
+				if not_dominated:
 					# Strategy is NOT dominated by other_strategy_index
 					other_strategy_index += 1
 					enemy_strategy_index = len(tab[0]) + 1
@@ -68,7 +83,8 @@ def get_not_dominated_strategies(tab):
 				# Strategy is dominated by other_strategy_index
 				logger.debug("%s < %s", strategy_index, other_strategy_index)
 				logger.debug("\n%s", np.array(tab))
-				result_list = rem(result_list, strategy_index)
+				# result_list = rem(result_list, strategy_index)
+				to_remove.append(strategy_index)
 				# other_strategy_index = 0
 				break
 			if same:
@@ -76,7 +92,7 @@ def get_not_dominated_strategies(tab):
 
 		strategy_index += 1
 
-	return result_list
+	return np.delete(tab, to_remove, 0)
 
 
 def find_min_and_max(tab):
@@ -103,8 +119,10 @@ def enum_states_from(n1, n2, t):
 def calculate_dist_and_utility(tab):
 	# Variables
 	variables = []
-	for i in range(len(tab)):
-		variables.append(LpVariable("p" + str(i), 0, 1))
+	distribution_indices = []
+	for i in range(1, len(tab)):
+		variables.append(LpVariable("p" + str(tab[i][0]), 0, 1))
+		distribution_indices.append(tab[i][0])
 	weights = tab
 
 	# Solving the problem from the vectors Variables and weights
@@ -117,10 +135,11 @@ def calculate_dist_and_utility(tab):
 	linear_problem += t
 
 	# Constraints
-	for column in range(len(weights[0])):
+	for column in range(1, len(weights[0])):
 		utility = 0
-		for row in range(len(weights)):
-			utility += variables[row] * weights[row][column]
+		for row in range(1, len(weights)):
+			logger.debug("\ntab: \n%s, row: %s, column: %s, variables: %s", weights, row, column, variables)
+			utility += variables[row - 1] * weights[row][column]
 		# print("UTILITY : ",utility)
 		linear_problem += utility - t >= 0
 
@@ -133,16 +152,16 @@ def calculate_dist_and_utility(tab):
 	# produce output only in DEBUG mode
 	linear_problem.solve(GLPK_CMD(msg=DEBUG))
 
-	distribution = []
+	distribution_list = []
 	# Solution
 	for v in linear_problem.variables():
 		# print(v.name, "=", v.varValue)
 		if v.name != "t":
-			distribution.append(v.varValue)
+			distribution_list.append(v.varValue)
 
 	# print("objective=", value(linear_problem.objective))
 
-	return distribution, value(linear_problem.objective)
+	return (distribution_list, distribution_indices), value(linear_problem.objective)
 
 
 # noinspection PyPep8Naming
@@ -192,42 +211,6 @@ def G(x, y, t):
 		return utility
 
 
-# # Here is the recurrence
-
-# # We enumerate all the reachable states
-# states = enum_states_from(x,y,t)
-
-# #If there is only one issue for the player 1, the profits will be the arg min of all the issues from the player 2
-# if x == 1:
-#     x,y,t = states[0][0]
-#     mini = G(x,y,t)
-#     for state in states[0]:
-#         x,y,t = state
-#         profits = G(x,y,t)
-#         if  profits < mini:
-#             mini = profits
-#     return mini
-
-# # If there is more the one issue for the player 1 we try to reduce the number of issues by finding dominated ones
-# p = []
-# for x1 in range(len(states)):
-#     p.append(0)
-#     for y in range(len(states[0])):
-#         for x2 in range(len(states)):
-#             if x1 < x2 :
-#                 p[x1] = 1
-
-# if len1(p) > -1:
-#     x,y,t = states[len1(p)][0]
-#     mini = G(x,y,t)
-#     for state in states[len1(p)]:
-#         x,y,t = state
-#         profits = G(x,y,t)
-#         if  profits < mini:
-#             mini = profits
-#     return mini
-
-
 def solve(x, y, t):
 	# We enumerate all the reachable states
 	states = enum_states_from(x, y, t)
@@ -250,34 +233,42 @@ def solve(x, y, t):
 			row[i] = G(x, y, t)
 
 	# We delete all the dominated strategies
-	not_dominated_strategy_indices = get_not_dominated_strategies(states)
+	# not_dominated_strategy_indices = get_not_dominated_strategies(states)
+	game = eliminate_dominated_strategies(np.array(states))
 
-	for i in range(len(states) - 1, -1, -1):
-		if i not in not_dominated_strategy_indices:
-			del states[i]
+	# for i in range(len(states) - 1, -1, -1):
+	# 	if i not in not_dominated_strategy_indices:
+	# 		del states[i]
 
-	return calculate_dist_and_utility(states)
+	return calculate_dist_and_utility(game)
 
 
-# print(Solve(5,4,-1))
+def represents_int(s):
+	try:
+		int(s)
+		return True
+	except ValueError:
+		return False
 
+
+# print(solve(5, 4, -1))
 
 if __name__ == "__main__":
 	if len(sys.argv) == 4:
 		try:
 			start_time = time.time()
-			(p, g) = solve(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+			((distribution, distribution_ind), g) = solve(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
 			print("--- %s seconds ---" % (time.time() - start_time))
 
-			p = np.array(p)
-			p /= p.sum()
-			print("Distribution:", list(p))
+			distribution = np.array(distribution)
+			distribution /= distribution.sum()
+			print("Distribution:", list(distribution), distribution_ind)
 			print("Utility: ", g)
-			X = np.random.choice(range(len(p)), 1, p=p)
-			print("You should shoot", X[0] + 1, "rocks.")
+			X = np.random.choice(distribution_ind, 1, p=distribution)
+			print("You should shoot", X[0], "rocks.")
 		except ValueError:
 			for index in range(1, len(sys.argv)):
-				if not sys.argv[index].isdigit():
+				if not represents_int(sys.argv[index]):
 					print("<", sys.argv[index], "> is not an integer")
 					print("Usage: Python3 Troll&Castles.py <x y t> where x,y,t are integers")
 					break
