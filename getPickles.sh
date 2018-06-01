a8452db233014154411ef411b505a4af6bfaf232
kalnar <kalnarobert@gmail.com>
2018-06-01 23:24:05 +0200

    fix get pickles

--- getPickles.sh
+++ getPickles.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir -p field{9..13..2}
-mkdir -p field{17..21..2}
+mkdir -p field9
+mkdir -p field11
+mkdir -p field13
+mkdir -p field17
+mkdir -p field19
+mkdir -p field21
 
-for i in {9..13..2} {17..21..2}: do
+
+for i in 9 11 13 17 19 21; do
   wget -P field${i} http://kalnar.eu/assets/game-theory/field${i}/distributions.pkl 
   wget -P field${i} http://kalnar.eu/assets/game-theory/field${i}/utilities.pkl 
 done
